6db83be7a6c6a25f16afee114d46d90d5dfeecda
Marcos Mainar <marcosmainar@gmail.com>
2013-10-20 18:20:56 +0100

    Stuck

--- WordWrapper/WordWrapperTest.cs
+++ WordWrapper/WordWrapperTest.cs
@@ -11,6 +11,7 @@ namespace WordWrapper
         {
             Assert.AreEqual("word\nword", Wrap("word word", 4));
             Assert.AreEqual("a dog", Wrap("a dog", 5));
+            Assert.AreEqual("a dog\nwith a\nbone", Wrap("a dog with a bone", 6));
         }
 
         private string Wrap(string s, int width)
