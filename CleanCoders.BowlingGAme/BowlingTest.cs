fcc80206e089667d4423364ca8ed32ebd782cd9c
Marcos Mainar <marcosmainar@gmail.com>
2013-10-22 21:36:27 +0100

    Bowling Game Kata - Episode 6 Part 2 - TDD

---
+++ CleanCoders.BowlingGAme/BowlingTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CleanCoders.BowlingGAme
+{
+    [TestClass]
+    public class BowlingTest
+    {
+        private Game g;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            g = new Game();
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                g.Roll(pins);
+        }
+
+        [TestMethod]
+        public void GutterGame()
+        {
+            RollMany(20, 0);
+            Assert.AreEqual(0, g.Score());
+        }
+
+        [TestMethod]
+        public void AllOnes()
+        {
+            RollMany(20, 1);
+            Assert.AreEqual(20, g.Score());
+        }
+
+        [TestMethod]
+        public void OneSpare()
+        {
+            g.Roll(5);
+            g.Roll(5); // Spare
+            g.Roll(3);
+            RollMany(17, 0);
+            Assert.AreEqual(16, g.Score());
+        }
+
+
+    }
+}
---
+++ CleanCoders.BowlingGAme/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCoders.BowlingGAme
+{
+    public class Game
+    {
+        private int score = 0;
+
+        public void Roll(int pins)
+        {
+            score += pins;
+        }
+
+        public int Score()
+        {
+            return score;
+        }
+    }
+}
