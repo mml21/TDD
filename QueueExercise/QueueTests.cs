1704e09271904d4a6a7274a07f81c8af32f6b123
Marcos Mainar <marcosmainar@gmail.com>
2013-11-23 15:45:35 +0000

    Queue exercise

---
+++ QueueExercise/QueueTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace QueueExercise
+{
+    [TestFixture]
+    public class QueueTests
+    {
+        private Queue queue;
+
+        [SetUp]
+        public void SetUp()
+        {
+            queue = new Queue();
+        }
+
+        [Test]
+        public void NewQueue_CreatesEmptyQueue()
+        {
+            Assert.AreEqual(0, queue.Size());
+        }
+
+        [Test]
+        public void AfterPushingOneElement_SizeIsOne()
+        {
+            queue.Push(0);
+            Assert.AreEqual(1, queue.Size());
+        }
+
+        [Test]
+        public void PopAfterPush_SizeIsZero()
+        {
+            queue.Push(0);
+            queue.Pop();
+            Assert.AreEqual(0, queue.Size());
+        }
+
+        [Test]
+        public void PopAfterPush_ReturnsSameElement()
+        {
+            queue.Push(0);
+            int element = queue.Pop();
+            Assert.AreEqual(0, element);
+        }
+
+        [Test]
+        public void TwoPopsAfterTwoPushes_SizeIsZero()
+        {
+            queue.Push(5);
+            queue.Push(10);
+            int element = queue.Pop();
+            element = queue.Pop();
+            Assert.AreEqual(0, queue.Size());
+        }
+
+        [Test]
+        [ExpectedException(typeof(Queue.UnderflowException))]
+        public void PopWhenEmptyQueue_ThrowsUnderflowException()
+        {
+            queue.Pop();
+        }
+
+        [Test]
+        public void TwoPopsAfterTwoPushes_ReturnsElementsInPushedOrder()
+        {
+            queue.Push(5);
+            queue.Push(10);
+            int element = queue.Pop();
+            Assert.AreEqual(5, element);
+            element = queue.Pop();
+            Assert.AreEqual(10, element);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Queue.OverflowException))]
+        public void MultiplePushesBeyondMaxSize_ThrowsOverflowException()
+        {
+            queue.Push(5);
+            queue.Push(10);
+            queue.Push(20);
+            queue.Push(40);
+        }
+
+        [Test]
+        public void MultiplePopsAndPushes()
+        {
+            queue.Push(5);
+            queue.Push(10);
+            queue.Push(20);
+
+            int element = queue.Pop();
+            Assert.AreEqual(5, element);
+            element = queue.Pop();
+            Assert.AreEqual(10, element);
+            element = queue.Pop();
+            Assert.AreEqual(20, element);
+
+            queue.Push(30);
+            element = queue.Pop();
+            Assert.AreEqual(30, element);
+
+            queue.Push(10);
+            queue.Push(20);
+            element = queue.Pop();
+            Assert.AreEqual(10, element);
+            element = queue.Pop();
+            Assert.AreEqual(20, element);
+        }
+
+    }
+
+    public class Queue
+    {
+        private int size = 0;
+        private static int MAX_SIZE = 3;
+        private int[] elements = new int[MAX_SIZE];
+        private int queueFront = 0;
+        private int queueBack = 0;
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public void Push(int i)
+        {
+            if (size == MAX_SIZE)
+                throw new OverflowException();
+
+            size++;
+
+            if (queueBack == MAX_SIZE && size < MAX_SIZE)
+            {
+                queueBack = 0;
+            }
+
+            elements[queueBack++] = i;
+        }
+
+        public int Pop()
+        {
+            if (size == 0)
+                throw new UnderflowException();
+
+            if (queueFront == MAX_SIZE && size < MAX_SIZE)
+            {
+                queueFront = 0;
+            }
+
+            --size;
+            return elements[queueFront++];
+        }
+
+        public class UnderflowException : Exception
+        {
+        }
+
+        public class OverflowException : Exception
+        {
+        }
+    }
+}
